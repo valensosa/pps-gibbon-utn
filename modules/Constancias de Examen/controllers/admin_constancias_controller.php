a02b2232623479aad4ef58b0fcae648104a5fddb
Valentina <vsosa@fi.uba.ar>
2026-02-01 15:03:36 -0300

    agrego admin constancias service

---
+++ modules/Constancias de Examen/controllers/admin_constancias_controller.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once __DIR__ . '/../services/AdminConstanciasService.php';
+require_once __DIR__ . '/../infrastructure/repositories/ConstanciasRepository.php';
+
+class AdminConstanciasController
+{
+    private AdminConstanciasService $service;
+    private $session;
+    private $page;
+    private $guid;
+
+    public function __construct(PDO $connection, $session, $page, $guid)
+    {
+        $repo = new ConstanciasRepository($connection);
+        $this->service = new AdminConstanciasService($repo);
+
+        $this->session = $session;
+        $this->page = $page;
+        $this->guid = $guid;
+    }
+
+    public function handle(): void
+    {
+        if (!isActionAccessible(
+            $this->guid,
+            $GLOBALS['connection2'],
+            '/modules/Constancias de Examen/adminView/admin_constancias.php'
+        )) {
+            $this->page->addError(__('No tiene acceso a esta acción.'));
+            return;
+        }
+
+        try {
+            $data = $this->service->getViewData();
+
+            $solicitudes = $data['solicitudes'];
+
+            require __DIR__ . '/../views/admin_constancias_view.php';
+
+        } catch (RuntimeException $e) {
+            $this->page->addError(__($e->getMessage()));
+        }
+    }
+}
