cefca99ad7f140d5b43c80d5760322984c600e0e
Valentina <vsosa@fi.uba.ar>
2026-02-08 22:20:50 -0300

    agrego conexion a gibbon para student

--- modules/Constancias de Examen/studentView/student_constancias.php
+++ modules/Constancias de Examen/studentView/student_constancias.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../../gibbon.php';
 
 require_once dirname(__DIR__) . '/moduleFunctions.php';
 require_once __DIR__ . '/../controllers/student_constancias_controller.php';
