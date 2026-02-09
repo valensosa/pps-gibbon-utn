539916b1585973961c56087070e40eba73d4fce0
Valentina <vsosa@fi.uba.ar>
2026-02-08 22:19:35 -0300

    agrego conexion gibbon

--- modules/Constancias de Examen/admin_constancias.php
+++ modules/Constancias de Examen/admin_constancias.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../gibbon.php';
+require_once __DIR__ . '/../../../gibbon.php';
 
 // Temporary redirect to adminView version
 include __DIR__ . '/adminView/admin_constancias.php'; 
