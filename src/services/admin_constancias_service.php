fa3d8f7f53133e2fda05cbf99bdea323b36cbfbc
Valentina <vsosa@fi.uba.ar>
2026-03-24 17:34:20 -0300

    modiico admin service

--- src/services/admin_constancias_service.php
+++ src/services/admin_constancias_service.php
@@ -3,25 +3,49 @@
 namespace App\Services;
 
 use App\Infrastructure\Repository\FirestoreRepository;
+use App\Infrastructure\Repository\StudentRepository;
 
 class AdminConstanciasService
 {
-    private FirestoreRepository $repo;
+    private FirestoreRepository $firestoreRepo;
+    private StudentRepository $studentRepo;
 
-    public function __construct(FirestoreRepository $repo)
+    public function __construct(FirestoreRepository $firestoreRepo, StudentRepository $studentRepo)
     {
-        $this->repo = $repo;
+        $this->firestoreRepo = $firestoreRepo;
+        $this->studentRepo   = $studentRepo;
     }
 
     public function getViewData(): array
     {
-        $docs = $this->repo->getAll();
+        $docs = $this->firestoreRepo->getAll();
 
         $solicitudes = array_map(function ($doc) {
             $data = FirestoreRepository::parseDocument($doc);
             $data['constanciaId'] = FirestoreRepository::getDocumentId($doc);
+
+            $data['email'] = '';
+            if (!empty($data['dniAlumno'])) {
+                $info = $this->studentRepo->getStudentInfoByDni($data['dniAlumno']);
+                if ($info) {
+                    $data['email'] = $info['email'];
+                }
+            }
+
             return $data;
         }, $docs);
+
+        usort($solicitudes, function ($a, $b) {
+            $statusOrder = ['pendiente' => 1, 'completado' => 2, 'rechazado' => 3];
+            $aOrder = $statusOrder[$a['estado']] ?? 99;
+            $bOrder = $statusOrder[$b['estado']] ?? 99;
+
+            if ($aOrder !== $bOrder) {
+                return $aOrder <=> $bOrder;
+            }
+
+            return strtotime($b['fechaPedido'] ?? 0) <=> strtotime($a['fechaPedido'] ?? 0);
+        });
 
         return ['solicitudes' => $solicitudes];
     }
