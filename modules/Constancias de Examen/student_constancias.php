19f558a411a69c667403796cc6c1df359de7c15b
Valentina <vsosa@fi.uba.ar>
2026-03-24 18:11:56 -0300

    modifico student constancias

--- modules/Constancias de Examen/student_constancias.php
+++ modules/Constancias de Examen/student_constancias.php
@@ -3,12 +3,6 @@
 require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
 
 use App\Controllers\StudentConstanciasController;
-use App\Infrastructure\Repository\FirestoreRepository;
-use App\Infrastructure\Repository\StudentRepository;
-
-$credentialsPath = __DIR__ . '/credentials/firebase-credentials.json';
-$firestoreRepo   = new FirestoreRepository($credentialsPath);
-$studentRepo     = new StudentRepository($connection2);
 
 $controller = new StudentConstanciasController(
     $connection2,
