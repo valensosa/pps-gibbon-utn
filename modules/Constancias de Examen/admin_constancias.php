d63bfb3000604db0a6558b2d3f3e6107581bade8
Valentina <vsosa@fi.uba.ar>
2026-03-24 18:16:29 -0300

    borro modulefunctions

--- modules/Constancias de Examen/admin_constancias.php
+++ modules/Constancias de Examen/admin_constancias.php
@@ -1,5 +1,21 @@
 <?php
-require_once __DIR__ . '/../../../gibbon.php';
 
-// Temporary redirect to adminView version
-include __DIR__ . '/adminView/admin_constancias.php'; 
+require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
+
+use App\Controllers\AdminConstanciasController;
+use App\Infrastructure\Repository\FirestoreRepository;
+use App\Infrastructure\Repository\StudentRepository;
+
+$credentialsPath = __DIR__ . '/credentials/firebase-credentials.json';
+$firestoreRepo   = new FirestoreRepository($credentialsPath);
+$studentRepo     = new StudentRepository($connection2);
+
+$controller = new AdminConstanciasController(
+    $session,
+    $page,
+    $guid,
+    $firestoreRepo,
+    $studentRepo
+);
+
+$controller->handle();
