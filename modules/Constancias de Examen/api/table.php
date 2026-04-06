fa9cb7a8d4fec15168af5aef665ce58c4d614747
Valentina <vsosa@fi.uba.ar>
2026-04-05 21:14:41 -0300

    agrego repo en table

--- modules/Constancias de Examen/api/table.php
+++ modules/Constancias de Examen/api/table.php
@@ -1,6 +1,14 @@
 <?php
-require_once dirname(__DIR__, 3) . '/gibbon.php';
-use App\controllers\TableController;
 
-$controller = new TableController();
+require_once dirname(__DIR__, 3) . '/gibbon.php';
+
+use App\controllers\TableController;
+use App\infrastructure\repository\StudentRepository;
+use App\infrastructure\repository\FirestoreRepository;
+
+$credentialsPath = dirname(__DIR__) . '/credentials/firebase-credentials.json';
+$studentRepo     = new StudentRepository($connection2);
+$firestoreRepo   = new FirestoreRepository($credentialsPath);
+
+$controller = new TableController($studentRepo, $firestoreRepo);
 echo $controller->handle();
